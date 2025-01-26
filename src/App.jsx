aef80c8227ff562ba98acf670401f99bca424cb7
Lenard Baluyot <lenardski@gmail.com>
2025-01-27 03:20:05 +0800

    Added holding dice mechanism

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,54 @@
 import Die from "./components/Die"
 import React from "react"
 import { useState } from "react"
+import { nanoid } from "nanoid"
 
 export default function App() {
 
   const [dice, setDice] = useState(generateAllNewDice())
+  const [number, setNumber] = useState(randomDiceNumber())
+
+  function randomDiceNumber() {
+    return Math.ceil(Math.random() * 6)
+  }
     
   function generateAllNewDice() {
       return new Array(10)
           .fill(0)
-          .map(() => ({ value: Math.ceil(Math.random() * 6), isHeld: true })
+          .map(() => 
+            ({ 
+              value: randomDiceNumber(),
+              isHeld: false,
+              id: nanoid() 
+            })
           )
   }
   
   function rollDices() {
       setDice(generateAllNewDice())
+      setNumber(randomDiceNumber())
   }
 
-  const diceElements = dice.map( (diceObject, index) => { return <Die key={index} diceObject={diceObject}/>})
+  function holdDice(id) {
+    setDice((currentDices) => {
+      return currentDices.map((dice) => {
+        return id === dice.id ? { ...dice, isHeld: !dice.isHeld } : dice
+      })
+    })
+  }
+
+
+  const diceElements = dice.map( (diceObject) => (
+    <Die 
+      key={diceObject.id}
+      diceObject={diceObject}
+      holdDice={holdDice} />
+  ))
 
   return (
     <main>
+      <h1>Tenzies</h1>
+      <h2>Current Number: {number}</h2>
       <div className="dice-container">
         {diceElements}
       </div>
--- src/components/Die.jsx
+++ src/components/Die.jsx
@@ -1,5 +1,10 @@
-export default function Die({diceObject}) {
+export default function Die(props) {
+
+    const handleHeldStyle = { backgroundColor: props.diceObject.isHeld ? "#59E391" : "white" }
+
     return (
-        <button>{diceObject.value}</button>
+        <button 
+        style={handleHeldStyle}
+        onClick={ () => props.holdDice(props.diceObject.id)}>{props.diceObject.value}</button>
     )
-}
+} 
