1650c1007f130fffb7b1d45c4f6cfaba07538ffd
Lenard Baluyot <lenardski@gmail.com>
2025-01-27 21:54:47 +0800

    Added working roll

--- src/App.jsx
+++ src/App.jsx
@@ -25,8 +25,11 @@ export default function App() {
   }
   
   function rollDices() {
-      setDice(generateAllNewDice())
-      setNumber(randomDiceNumber())
+    setDice( (currentDices) => {
+      return currentDices.map( (dice) => {
+        return !dice.isHeld ? {...dice, value: randomDiceNumber() } : dice
+      })
+    })
   }
 
   function holdDice(id) {
@@ -47,8 +50,8 @@ export default function App() {
 
   return (
     <main>
-      <h1>Tenzies</h1>
-      <h2>Current Number: {number}</h2>
+      <h1 className="title">Tenzies</h1>
+      <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
       <div className="dice-container">
         {diceElements}
       </div>
